a79a0f00414d14e5f3e2d7f9f5c7875621b7000a
iliabaan <iliabaan@mail.ru>
2020-05-31 21:31:06 +0300

    q

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,38 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <headerMain />
+    <router-view class="container"></router-view>
+    <footerMain />
   </div>
 </template>
 
+<script>
+import headerMain from '@/components/headerMain.vue';
+import footerMain from '@/components/footerMain.vue';
+
+export default {
+  name: 'App',
+  components: {
+    headerMain,
+    footerMain,
+  },
+};
+</script>
+
 <style>
+
+* {
+  margin: 0;
+  padding: 0;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: Lato, sans-serif;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
 }
 </style>
